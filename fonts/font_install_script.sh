9a12ccc59552a39d5069fcf95faa90800389fbc4
Daniel Robson <danr.236@gmail.com>
2022-06-27 21:19:25 -0400

    init commit

---
+++ fonts/font_install_script.sh
@@ -0,0 +1,37 @@
+otftotfm -a -e texnansx "Linotype - AvenirNextLTPro-BoldIt.otf" \
+        -fkern -fliga LY1--AvenirNextLTPro-BoldIt
+otftotfm -a -e texnansx "Linotype - AvenirNextLTPro-Demi.otf" \
+        -fkern -fliga LY1--AvenirNextLTPro-Demi
+otftotfm -a -e texnansx "Linotype - AvenirNextLTPro-DemiIt.otf" \
+        -fkern -fliga LY1--AvenirNextLTPro-DemiIt
+otftotfm -a -e texnansx "Linotype - AvenirNextLTPro-Heavy.otf" \
+        -fkern -fliga LY1--AvenirNextLTPro-Heavy
+otftotfm -a -e texnansx "Linotype - AvenirNextLTPro-HeavyIt.otf" \
+        -fkern -fliga LY1--AvenirNextLTPro-HeavyIt
+otftotfm -a -e texnansx "Linotype - AvenirNextLTPro-It.otf" \
+        -fkern -fliga LY1--AvenirNextLTPro-It
+otftotfm -a -e texnansx "Linotype - AvenirNextLTPro-Light.otf" \
+        -fkern -fliga LY1--AvenirNextLTPro-Light
+otftotfm -a -e texnansx "Linotype - AvenirNextLTPro-LightIt.otf" \
+        -fkern -fliga LY1--AvenirNextLTPro-LightIt
+otftotfm -a -e texnansx "Linotype - AvenirNextLTPro-MediumIt.otf" \
+        -fkern -fliga LY1--AvenirNextLTPro-MediumIt
+otftotfm -a -e texnansx "Linotype - AvenirNextLTPro-Thin.otf" \
+        -fkern -fliga LY1--AvenirNextLTPro-Thin
+otftotfm -a -e texnansx "Linotype - AvenirNextLTPro-ThinIt.otf" \
+        -fkern -fliga LY1--AvenirNextLTPro-ThinIt
+otftotfm -a -e texnansx "Linotype - AvenirNextLTPro-UltLt.otf" \
+        -fkern -fliga LY1--AvenirNextLTPro-UltLt
+otftotfm -a -e texnansx "Linotype - AvenirNextLTPro-UltLtIt.otf" \
+        -fkern -fliga LY1--AvenirNextLTPro-UltLtIt
+otftotfm -a -e texnansx "Linotype-AvenirNextLTPro-Bold.otf" \
+        -fkern -fliga LY1--AvenirNextLTPro-Bold
+otftotfm -a -e texnansx "Linotype-AvenirNextLTPro-Medium.otf" \
+        -fkern -fliga LY1--AvenirNextLTPro-Medium
+otftotfm -a -e texnansx "Linotype-AvenirNextLTPro-Regular.otf" \
+        -fkern -fliga LY1--AvenirNextLTPro-Regular
+
+otftotfm -a -e texnansx "Linotype-AvenirNextLTPro-Regular.otf" \
+        -fkern -fliga -fsmcp LY1--AvenirNextLTPro-Regular--fsmcp
+otftotfm -a -e texnansx "Linotype-AvenirNextLTPro-Bold.otf" \
+        -fkern -fliga -fsmcp LY1--AvenirNextLTPro-Bold--fsmcp
